a3a25cbdeece90ab3c8567f8fc6bff25334777a6
Filip Vabrousek <filipvabrousek1@gmail.com>
2019-10-01 09:10:20 +0200

    UI Improvements

--- AdvancedUI/AdvancedUI/Geometry.swift
+++ AdvancedUI/AdvancedUI/Geometry.swift
@@ -9,42 +9,59 @@
 import SwiftUI
 
 struct GProxy: View {
+
+    @State var progress: CGFloat = 0.0
+    @State var text: String = "Set progress"
+
     var body: some View {
         VStack {
             Text("GeometryProxy").bold().edgesIgnoringSafeArea(.top).padding(13.0)
-            BarView(value: 0.33)
+
+            Spacer()
+
+            Button(text) {
+                withAnimation {
+                    if self.progress > 0.0 {
+                        self.progress = 0.0
+                        self.text = "Set progress"
+                    } else {
+                        self.progress += 0.33
+                        self.text = "Reset"
+
+                    }
+                }
+            }.frame(width: UIScreen.main.bounds.width)
+
+            Spacer()
+            BarView(value: $progress)
+            Spacer()
         }
     }
 }
 
-struct GeometryEf: View {
-    var body: some View {
-        VStack {
-            Text("GeometryEffect").bold().edgesIgnoringSafeArea(.top).padding(13.0)
-            Spacer()
-            Text("Skewed")
-                .background(Color.green)
-                .padding()
-                .modifier(Skew(offset: 0.6, skew: 0.4))
-            Spacer()
-        }
-    }
-}
 
 struct BarView: View {
-    @State var value: CGFloat = 0.0
-
+    @Binding var value: CGFloat
+    
     var body: some View {
         GeometryReader { g in
-            VStack(alignment: .trailing) {
-                Text("Progress: \(self.value)").padding()
+            VStack() {
+                Text("\(Int(self.value * 100)) %")
+               // Text(self.formatTime())
+                    .edgesIgnoringSafeArea(.leading)
+                    .edgesIgnoringSafeArea(.trailing)
+                    .padding()
 
                 ZStack(alignment: .leading) {
                     Rectangle().opacity(0.1).clipShape(RoundedRectangle(cornerRadius: 6.0))
-                    Rectangle().frame(minWidth: 0, idealWidth: self.getWidth(proxy: g),
+                    Rectangle().background(Color(0x1abc9c)).frame(minWidth: 0, idealWidth: self.getWidth(proxy: g),
                         maxWidth: self.getWidth(proxy: g))
-                        .background(Color(0x1abc9c)) // After clip shape
-                    .clipShape(RoundedRectangle(cornerRadius: 6.0))
+
+                    //  .overlay(RoundedRectangle(cornerRadius: 6.0).background(Color(0x1abc9c)))
+
+                        .clipShape(RoundedRectangle(cornerRadius: 6.0))
+
+                    // After clip shape
 
                 }.frame(height: 10)
 
@@ -58,6 +75,46 @@ struct BarView: View {
         return frame.size.width * value
     }
 }
+
+
+
+struct GeometryEf: View {
+    @State var offset: CGFloat = 0.0
+    @State var skew: CGFloat = 0.0
+    @State var skewed: Bool = false
+
+    var body: some View {
+        VStack {
+            Text("GeometryEffect").bold().frame(width: 200).edgesIgnoringSafeArea(.top).padding(.top, 13.0)
+            Spacer()
+
+            Text("Skewed")
+                .font(.system(size: 40, weight: .heavy, design: .default))
+                .background(Color.green)
+                .padding(10)
+                .modifier(Skew(offset: offset, skew: skew))
+
+            Spacer()
+
+            Button("Skew") {
+                withAnimation {
+                    if self.skewed {
+                        self.offset = 0.0
+                        self.skew = 0.0
+                        self.skewed.toggle()
+                    } else {
+                        self.offset = 0.6
+                        self.skew = 0.4
+                        self.skewed.toggle()
+                    }
+                }
+            }
+            Spacer()
+        }
+    }
+}
+
+
 
 // Skew using GeometryEffect
 struct Skew: GeometryEffect {
